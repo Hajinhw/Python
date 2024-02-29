97c4a8d1170d0c04e54b015b8909c7cc66f2c39a
hajin <gkwlsxn77@naver.com>
2024-02-29 17:52:18 +0900

    0229 algorithm

---
+++ 알고리즘/11_02_선택정렬.py
@@ -0,0 +1,20 @@
+## 함수
+from random import randint
+def  findMinIndex(ary) :
+    minIdx = 0
+    for i in range(1, len(ary), 1) :
+        if (ary[minIdx] > ary[i]) :
+            minIdx = i
+    return minIdx
+
+## 변수
+before = [ randint(30, 190) for _ in range(8)]
+after = []
+
+## 메인
+print('정렬 전-->', before)
+for _ in range(len(before)) :
+    minPos = findMinIndex(before)
+    after.append(before[minPos])
+    del (before[minPos])
+print('정렬 후-->', after)
